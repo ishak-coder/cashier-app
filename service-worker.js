1e5fc79868004f1e0b44c0b327e201323338eed5
ishak-coder <muwangaishak58@gmail.com>
2025-05-04 18:24:21 +0300

    service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,18 @@
+const CACHE_NAME = 'cashier-app-cache-v1';
+const urlsToCache = ['/', '/index.html', '/styles.css', '/script.js'];
+
+self.addEventListener('install', function (event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      return response || fetch(event.request);
+    })
+  );
+});
